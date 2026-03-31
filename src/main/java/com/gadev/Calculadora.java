15e81c1a0e6256a0490f003db4c67861611bb009
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-30 21:52:23 -0500

    feat: implement basic calculator functionality with menu interface

--- src/main/java/com/gadev/Calculadora.java
+++ src/main/java/com/gadev/Calculadora.java
@@ -1,4 +1,33 @@
 package com.gadev;
 
 public class Calculadora {
+    private double num1;
+    private double num2;
+
+    public Calculadora(){};
+
+    public Calculadora(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double calcularSuma(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double calcularResta(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double calcularMultiplicacion(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double calcularDivision(double num1, double num2) {
+        if (num2 == 0){
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return num1 / num2;
+    }
+
 }
--- src/main/java/com/gadev/MenuCalculadora.java
+++ src/main/java/com/gadev/MenuCalculadora.java
@@ -1,5 +1,48 @@
 package com.gadev;
 
+import java.util.Scanner;
+
 public class MenuCalculadora {
+
+    Scanner scanner = new Scanner(System.in);
+    Calculadora calc = new Calculadora();
+
+    public void start(){
+
+        boolean continuar = true;
+
+        while(continuar){
+            printMenu();
+            int command = scanner.nextInt();
+
+            switch (command){
+                case 1:
+                    System.out.println(calc.calcularSuma(n1,n2)); break;
+                case 2: System.out.println(calc.calcularResta(n1,n2)); break;
+                case 3: System.out.println(calc.calcularMultiplicacion(n1,n2)); break;
+                case 4: try {
+                    System.out.println(calc.calcularDivision(n1,n2)); break;
+                } catch (ArithmeticException e){
+                    System.out.println("Error: " + e.getMessage());
+                }; break;
+                case 5: continuar = false; break;
+                default:
+                    System.out.println("Ingrese un comando valido");
+            }
+
+        }
+    }
+
+    public void printMenu(){
+        System.out.println("CALCULADORA");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Division");
+        System.out.println("5. Salir");
+    }
+
+
+
 
 }
